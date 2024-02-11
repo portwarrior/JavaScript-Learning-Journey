6e14f992854bf51b175a92bce4e30a16c756f00b
pixilised <bilaltosif123@gmail.com>
2024-02-11 14:57:56 +1100

    Initial commit

---
+++ Level 1 part a/demo.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+console.log("hello worldaaaa")
+let a=52
+console.log(a)
---
+++ Level 1 part a/var_let.js
@@ -0,0 +1,5 @@
+console.log("Lets talk about var and let and constant")
+// var is globally scoped and let and const are block scoped 
+
+var a=45
+let b=52
