d14fda2eef343e27dc7b0a863f4266786373f6d4
pixilised <bilaltosif123@gmail.com>
2024-02-12 22:38:26 +1100

    002 commit
    Practice questions on stuff done so far

---
+++ Level 1 part a/Practice questions.js
@@ -0,0 +1,69 @@
+//note when ur using prompt use it in browser or replit cuz this will only work on a website. vscode/nodejs wont allow it to work as a result this will throw erorrs.
+
+
+// let age=prompt("what is your age")
+// age=Number.parseInt(age)
+// if(age>=10 && age <20){
+//     alert("The age is between 10 and 20")
+// }
+
+// else{
+//     alert("No the age is not between 10 and 20")
+// }
+
+//---------------
+
+//switch case statements
+// let age=prompt("what is your age")
+// switch(age){
+//     case "12":
+//         console.log("Your age is 12")
+//         break
+//     case "13":
+//         console.log("Your age is 13")
+//         break //writing this ensures all the cases below this dont run
+//     case "14":
+//         console.log("Your age is 14")
+//         break
+//     case "15":
+//         console.log("Your age is 15")
+//         break
+//     case "16":
+//         console.log("Your age is 16")
+//         break
+//     default:
+//         console.log("Your age is not special")
+// }
+
+//----------
+//wap that shwos a numbr is divisible by 2 and 3
+
+// let age=prompt("what is your age")
+// age=Number.parseInt(age)
+// if(age%2===0 && age%3===0){
+//     alert("The number is divisible by 2 and 3")
+// }
+
+// else{
+//     alert("The number is not divisible by 2 and 3")
+// }
+
+//------------------
+//WAP JS program o show that a number is either divisible by either 2 or 3
+
+// let age=prompt("what is your age")
+// age=Number.parseInt(age)
+// if(age%2===0 || age%3===0){
+//     alert("The number is divisible by 2 and 3")
+// }
+
+// else{
+//     alert("The number is not divisible by 2 and 3")
+// }
+
+//----------------
+//using ternay operator print "you can drive" or not based on age being greater than 18 
+
+let ahe=19
+let a=age>18?"you can drive":"you cannot drive"
+console.log(a)
