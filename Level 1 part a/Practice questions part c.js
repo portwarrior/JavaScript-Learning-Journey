6c44ce67f15f36b9c0a53ee2f4e3aa4fae8c44a0
pixilised <bilaltosif123@gmail.com>
2024-02-14 16:14:00 +1100

    Commit 007
    Practie questions on strings

---
+++ Level 1 part a/Practice questions part c.js
@@ -0,0 +1,18 @@
+let a="Bettlejuice"
+let word="ttle"
+console.log(a.includes("B"))
+console.log(a.includes("b"))
+console.log(a.startsWith("B"))
+console.log(a.startsWith("b"))
+console.log(a.endsWith("e"))
+console.log(a.endsWith("E"))
+console.log(a.toLowerCase())
+
+
+//wap to pull out 1000 from the string
+let sentence="Please give dollars 1000" 
+let amount=Number.parseInt(sentence.slice(20))
+console.log(amount)
+
+
+ 
