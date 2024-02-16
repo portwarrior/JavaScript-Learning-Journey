10998aa4128a4728dfd2df1d4477b87212eaacc1
pixilised <bilaltosif123@gmail.com>
2024-02-17 00:31:44 +1100

    DOM
    Accessing Child nodes

--- Level 2 part a/Part a/script.js
+++ Level 2 part a/Part a/script.js
@@ -1,1 +1,12 @@
 alert("Welcome to the page fam. Its an honour to have you here")
+
+
+//we are trying to access the first child of te body
+console.log(document.body.firstChild)//first child
+console.log(document.body.lastChild)//last child
+console.log(document.body.childNodes)//all child notes
+
+
+//Element.childNodes[0]===Element.firstChild
+//Element.childNodes[Element.childNodes.length-1]===Element.lastChild
+//Element.hasChildNodes() cheks whether there are are any child nodes
---
+++ Level 2 part a/Part b/Project 2.js
@@ -0,0 +1,2 @@
+//wap to creae a game of snake,water,gun. the game should ask you to enter s,w,g. the computer should be able to randomly generate s,w or g and declare win or loss using alert. Use confirm and prompt whenever required
+
