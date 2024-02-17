9ad82b95815ed8b5a4e4dddf1915b45cf79f0700
pixilised <bilaltosif123@gmail.com>
2024-02-17 15:48:56 +1100

    Commit 016
    Understanding using tables

---
+++ Level 2 part a/Part a/DOM project/Project c/script.js
@@ -0,0 +1,10 @@
+let t=document.body.firstElementChild.firstElementChild
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tFoot.firstElementChild)
+console.log(t.tBody)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
+//the type of document and type of window is same which is "Object"
+
