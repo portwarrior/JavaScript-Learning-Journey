dfe8cf9aac327a82c0e4a438f27d2f086159d2fc
pixilised <bilaltosif123@gmail.com>
2024-02-26 14:35:25 +1100

    updated

--- Level 2 part a/Part c/Promises/Promise chaining/Promise chaining pt2/script.js
+++ Level 2 part a/Part c/Promises/Promise chaining/Promise chaining pt2/script.js
@@ -14,4 +14,6 @@ const loadScript = (src) => {
 let p2 = loadScript("http://localhost:3000/index.js")
   p1.then((value) => {
     console.log(value)
+  }).catch((error)=>{
+    console.log("We are sorry we are having problems loading the script")
   })
