93dd9d47be28829b893f6f52f896e6fc772a65e0
pixilised <bilaltosif123@gmail.com>
2024-02-26 14:31:59 +1100

    Updated

--- Level 2 part a/Part c/Promises/Promise chaining/Promise chaining pt2/script.js
+++ Level 2 part a/Part c/Promises/Promise chaining/Promise chaining pt2/script.js
@@ -1,15 +1,15 @@
 const loadScript = (src) => {
-    return new Promise(resolve => {
+    return new Promise((resolve,reject)=> {
         let script = document.createElement("script")
         script.type = "text/javascript"
         script.src = src
         document.body.appendChild(script)
         script.onLoad = () => {
-        resolve(1)
+            resolve(1)
         }
         script.onerror = () => { reject(0) }
-    }    )
-    }
+    }  )
+}
   
 let p2 = loadScript("http://localhost:3000/index.js")
   p1.then((value) => {
